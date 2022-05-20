0025de052178cfa8e6032429675a04a612123026
Lachee <lachlan.h@hotmail.com>
2022-05-20 18:08:25 +1000

    Fixed MeshBuilder namespace

--- Utilities/MeshBuilder.cs
+++ Utilities/MeshBuilder.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-namespace Lachee.UtilitiesAssets.Lachee.Utilities
+namespace Lachee.Utilities
 {
     /// <summary>
     /// The MeshBuilder allows for easier manipulation and creation of meshes.
