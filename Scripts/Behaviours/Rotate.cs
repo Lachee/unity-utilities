5eb9ac96149b6670434a7141dba3d30a928a8f46
Lachee <lachlan.h@hotmail.com>
2022-05-22 19:42:43 +1000

    Moved rotate to Behaviours

--- Scripts/Behaviours/Rotate.cs
+++ Scripts/Behaviours/Rotate.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace Lachee.Utilities
+namespace Lachee.Behaviours
 {
     /// <summary>
     /// Spins an object at the given speed
