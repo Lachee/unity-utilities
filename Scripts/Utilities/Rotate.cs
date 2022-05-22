4c9ff827ea45942f32aae3663e38ad1889221f92
Lachee <lachlan.h@hotmail.com>
2022-05-22 19:12:42 +1000

    Added rotation graphic

--- Scripts/Utilities/Rotate.cs
+++ Scripts/Utilities/Rotate.cs
@@ -6,7 +6,7 @@ namespace Lachee.Utilities
     /// <summary>
     /// Spins an object at the given speed
     /// </summary>
-    public class Spin : MonoBehaviour
+    public class Rotate : MonoBehaviour
     {
         public Vector3 speed = Vector3.one;
         private void Update()
