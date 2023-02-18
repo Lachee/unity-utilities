a3b570d3401b83dd54ac4a09d14200d0c85297f5
Lachee <lachlan.h@hotmail.com>
2023-02-18 16:50:29 +1100

    added label

---
+++ Editor/Attributes/LabelDrawer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Lachee.Attributes.Editor
+{
+    [CustomPropertyDrawer(typeof(LabelAttribute))]
+    public class LabelDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            LabelAttribute attr = (LabelAttribute)attribute;
+            EditorGUI.PropertyField(position, property, attr.label, true);
+        }
+    }
+}
---
+++ Runtime/Attributes/LabelAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace Lachee.Attributes
+{
+    /// <summary>
+    /// Makes object readonly in the editor. 
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Class | AttributeTargets.Struct, Inherited = true)]
+    public class LabelAttribute : PropertyAttribute
+    {
+        /// <summary>
+        /// The label and tooltip for the property.
+        /// </summary>
+        public GUIContent label;
+
+        /// <summary>
+        /// Creates a label
+        /// </summary>
+        /// <param name="label">The name of the property</param>
+        /// <param name="tooltip">The tooltip for the property</param>
+        public LabelAttribute(string label, string tooltip = "")
+        {
+            this.label = new GUIContent(label, tooltip);
+        }
+
+        /// <summary>
+        /// Creates a new label from the GUIContent
+        /// </summary>
+        /// <param name="content"></param>
+        public LabelAttribute(GUIContent content)
+        {
+            this.label = content;
+        }
+    }
+}
