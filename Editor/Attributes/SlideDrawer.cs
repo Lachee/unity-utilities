4982f58a26ea9ac6dcb714d4acf3449ef350244f
Lachee <lachlan.h@hotmail.com>
2023-04-25 13:10:41 +1000

    Added support for sliders. Use the `[Slider]` attribute in place of `Range` for MinMax support on Vector2 and also labeling support.

---
+++ Editor/Attributes/SlideDrawer.cs
@@ -0,0 +1,95 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Lachee.Attributes.Editor
+{
+    [CustomPropertyDrawer(typeof(SlideAttribute))]
+    public class SlideDrawer : PropertyDrawer
+    {
+        const float miniLabelVerticalOffset = -7;
+
+        static GUIStyle miniLabelStyle;
+        static SlideDrawer()
+        {
+            miniLabelStyle = new GUIStyle(EditorStyles.miniLabel);
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            SlideAttribute attr = (SlideAttribute)attribute;
+
+
+            // Draw the left and right
+            if (attr.LeftLabel != null)
+            {
+                miniLabelStyle.alignment = TextAnchor.UpperLeft;
+                EditorGUI.LabelField(new Rect(position.x + EditorGUIUtility.labelWidth, position.y + miniLabelVerticalOffset, position.width, position.height), attr.LeftLabel, miniLabelStyle);
+            }
+
+            //style.alignment = TextAnchor.LowerRight;
+            if (attr.RightLabel != null)
+            {
+                float padding = property.propertyType == SerializedPropertyType.Float || property.propertyType == SerializedPropertyType.Integer ? -EditorGUIUtility.fieldWidth - 5 : 0;
+                miniLabelStyle.alignment = TextAnchor.UpperRight;
+                EditorGUI.LabelField(new Rect(position.x + EditorGUIUtility.labelWidth, position.y + miniLabelVerticalOffset, position.width - EditorGUIUtility.labelWidth + padding, position.height), attr.RightLabel, miniLabelStyle);
+            }
+
+            Rect sliderBox = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
+
+            // Draw the slider
+            switch (property.propertyType)
+            {
+                case SerializedPropertyType.Float:
+                    EditorGUI.Slider(sliderBox, property, attr.Min, attr.Max, label);
+                    break;
+                case SerializedPropertyType.Integer:
+                    EditorGUI.IntSlider(sliderBox, property, (int) attr.Min, (int) attr.Max, label);
+                    break;
+                case SerializedPropertyType.Vector2:
+                case SerializedPropertyType.Vector2Int:
+                    MinMaxSlider(sliderBox, property, attr.Min, attr.Max, label);
+                    break;
+                default:
+                    EditorGUI.LabelField(position, label, new GUIContent("Slider must be of type float, int, Vector2, or Vector2Int"));
+                    return;
+            }
+
+        }
+
+        private static void MinMaxSlider(Rect position, SerializedProperty property, float min, float max, GUIContent label)
+        {
+            float minValue = 0;
+            float maxValue = 0;
+
+            if (property.propertyType == SerializedPropertyType.Vector2)
+            {
+                minValue = property.vector2Value.x;
+                maxValue = property.vector2Value.y;
+                EditorGUI.MinMaxSlider(position, label, ref minValue, ref maxValue, min, max);
+                property.vector2Value = new Vector2(minValue, maxValue);
+            } 
+            else if (property.propertyType == SerializedPropertyType.Vector2Int)
+            {
+                minValue = property.vector2IntValue.x;
+                maxValue = property.vector2IntValue.y;
+                EditorGUI.MinMaxSlider(position, label, ref minValue, ref maxValue, min, max);
+                property.vector2IntValue = new Vector2Int(Mathf.RoundToInt(minValue), Mathf.RoundToInt(maxValue));
+            }
+            else
+            {
+                throw new System.ArgumentException("The property must be a vector2 type");
+            }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            float height = base.GetPropertyHeight(property, label);
+
+            SlideAttribute attr = attribute as SlideAttribute;
+            if (attr != null && (attr.LeftLabel != null || attr.RightLabel != null))
+                height += -miniLabelVerticalOffset;
+
+            return height;
+        }
+    }
+}
---
+++ Runtime/Attributes/SlideAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Lachee.Attributes
+{
+    [System.AttributeUsage(System.AttributeTargets.Field, AllowMultiple = false)]
+    public sealed class SlideAttribute : PropertyAttribute
+    {
+        public float Min { get; }
+        public float Max { get; }
+
+        public GUIContent LeftLabel { get; }
+        public GUIContent RightLabel { get; }
+
+        public SlideAttribute(float min, float max)
+        {
+            this.Min = min;
+            this.Max = max;
+            LeftLabel = null;
+            RightLabel = null;
+        }
+
+        public SlideAttribute(float min, float max, string leftLabel, string rightLabel)
+            : this(min, max, new GUIContent(leftLabel), new GUIContent(rightLabel)) { }
+
+        public SlideAttribute(float min, float max, GUIContent leftLabel, GUIContent rightLabel)
+        {
+            this.Min = min;
+            this.Max = max;
+            this.LeftLabel = leftLabel;
+            this.RightLabel = rightLabel;
+        }
+    }
+}
