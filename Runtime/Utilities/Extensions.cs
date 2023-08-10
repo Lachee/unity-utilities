b37f2f98bdadbfd718eaf3935e3edc716cb44e4f
Lachee <lachlan.h@hotmail.com>
2023-08-10 16:33:38 +1000

    added RectTransform extensions

--- Runtime/Utilities/Extensions.cs
+++ Runtime/Utilities/Extensions.cs
@@ -52,6 +52,30 @@ namespace Lachee.Utilities
             Vector2 size = Vector2.Scale(transform.rect.size, transform.lossyScale);
             return new Rect((Vector2)transform.position - (size * 0.5f), size);
         }
+
+        public static RectTransform SetLeft(this RectTransform rt, float left)
+        {
+            rt.offsetMin = new Vector2(left, rt.offsetMin.y);
+            return rt;
+        }
+
+        public static RectTransform SetRight(this RectTransform rt, float right)
+        {
+            rt.offsetMax = new Vector2(-right, rt.offsetMax.y);
+            return rt;
+        }
+
+        public static RectTransform SetTop(this RectTransform rt, float top)
+        {
+            rt.offsetMax = new Vector2(rt.offsetMax.x, -top);
+            return rt;
+        }
+
+        public static RectTransform SetBottom(this RectTransform rt, float bottom)
+        {
+            rt.offsetMin = new Vector2(rt.offsetMin.x, bottom);
+            return rt;
+        }
     }
 
     /// <summary>
